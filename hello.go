403da20045c20a37302f6e142568dbe03783fdd9
TomozQ <inf.4clover@gmail.com>
2022-06-09 19:07:18 +0900

    ショートステートメント付if分

--- hello.go
+++ hello.go
@@ -8,15 +8,26 @@ import (
 
 func main() {
 	x := hello.Input("type a number")
-	n, err := strconv.Atoi(x)
-	if err != nil {
-		fmt.Println("ERROR!!")
-		return
-	}
-	fmt.Print(x + "は、")
-	if n%2 == 0 {
-		fmt.Println("偶数です")
+	// n, err := strconv.Atoi(x)
+
+	// ifに入るとまずn, err := strconv.Atoi(x)が実行される。errがnilの場合には偶数奇数の判定に入る。
+	if n, err := strconv.Atoi(x); err == nil {
+		if n%2 == 0 {
+			fmt.Println("偶数です。")
+		} else {
+			fmt.Println("奇数です。")
+		}
 	} else {
-		fmt.Println("奇数です")
+		fmt.Println("整数ではありません。")
 	}
+	// if err != nil {
+	// 	fmt.Println("ERROR!!")
+	// 	return
+	// }
+	// fmt.Print(x + "は、")
+	// if n%2 == 0 {
+	// 	fmt.Println("偶数です")
+	// } else {
+	// 	fmt.Println("奇数です")
+	// }
 }
