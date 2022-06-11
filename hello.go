7f2d75a44574b8980e5d80d6ccf9a9c6c34c1d49
TomozQ <inf.4clover@gmail.com>
2022-06-12 00:49:26 +0900

    array basic

--- hello.go
+++ hello.go
@@ -4,21 +4,27 @@ import (
 	"fmt"
 	"hello"
 	"strconv"
+	"strings"
 )
 
 func main() {
+	x := hello.Input("input data")
+	// 入力された値を半角スペースで区切って配列にする ex)[10 20 30]
+	ar := strings.Split(x, " ")
+	// fmt.Println(ar)
 	t := 0
-	x := hello.Input("type a number")
-	n, err := strconv.Atoi(x)
-	if err != nil {
-		goto err //errラベルの処理にジャンプする
-	}
-	for i := 1; i <= n; i++ {
-		t += i
+	for i := 0; i < len(ar); i++ { //len(ar)配列の要素数
+
+		n, er := strconv.Atoi(ar[i])
+
+		if er != nil {
+			goto err
+		}
+
+		t += n
 	}
 	fmt.Println("total: ", t)
 	return
-
-err: //ラベル
+err:
 	fmt.Println("ERROR!")
 }
