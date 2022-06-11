a05d4b1a8cfe326814194e14a200b87dd3d4beb9
TomozQ <inf.4clover@gmail.com>
2022-06-11 20:04:41 +0900

    continue and break

--- hello.go
+++ hello.go
@@ -15,8 +15,16 @@ func main() {
 		return
 	}
 	t := 0
-	for i := 1; i <= n; i++ {
-		t += i
+	c := 0
+	for {
+		c++
+		if c%2 == 1 {
+			continue
+		}
+		if c > n {
+			break
+		}
+		t += c
 	}
 	fmt.Println(t, "です。")
 }
