235493bfac4e6e441ec8aee11edf76702bd1a3f8
TomozQ <inf.4clover@gmail.com>
2022-06-12 23:03:46 +0900

    slice operation

--- hello.go
+++ hello.go
@@ -3,21 +3,74 @@ package main
 import "fmt"
 
 func main() {
-	a := [3]int{10, 20, 30}
-	b := a[0:2]
+	a := []int{10, 20, 30}
 	fmt.Println(a)
-	fmt.Println(b)
-	b = append(b, 1000)
+
+	a = push(a, 1000)
 	fmt.Println(a)
-	fmt.Println(b)
-	b = append(b, 1000)
+
+	a = pop(a)
 	fmt.Println(a)
-	fmt.Println(b)
-	// 出力----
-	// [10 20 30] a
-	// [10 20] b
-	// [10 20 1000] a 値が変更されている
-	// [10 20 1000] b
-	// [10 20 1000] a 値が追加されていない
-	// [10 20 1000 1000] b 値が追加されている
+
+	a = unshift(a, 10000)
+	fmt.Println(a)
+
+	a = shift(a)
+	fmt.Println(a)
+
+	a = insert(a, 1000, 2)
+	fmt.Println(a)
+
+	a = remove(a, 2)
+	fmt.Println(a)
 }
+
+// 最後に追加
+func push(a []int, v int) []int {
+	return append(a, v)
+}
+
+// 最後を削除
+func pop(a []int) []int {
+	return a[:len(a)-1]
+}
+
+// 最初に追加
+func unshift(a []int, v int) []int {
+	// ... -> スライスを展開する =>a[0], a[1], a[2]
+	return append([]int{v}, a...) //vが入ったスライスを用意し、第2引数にaを展開したものを指定する。 => [v, a[0], a[1], a[2]]
+}
+
+// 最初を削除
+func shift(a []int) []int {
+	return a[1:] //インデックス番号1から後のみを返却
+}
+
+// 指定の位置に追加
+func insert(a []int, v int, p int) []int {
+	//a = [10, 20, 30]
+	//	aの最後に0を追加
+	a = append(a, 0)
+	//a = [10, 20, 30, 0]
+	//v -> 1000, p -> 2
+	a = append(a[:p+1], a[p:len(a)-1]...) //a[:3] -> [10, 20, 30], a[2:3]... -> 30
+	//a = [10, 20, 30, 30]
+	a[p] = v
+	// a[p] -> a[2] -> 30 = 1000
+	// a = [10, 20, 1000, 30]
+	return a
+}
+
+// 指定の位置を削除
+func remove(a []int, p int) []int {
+	return append(a[:p], a[p+1:]...) //a[:p] ->[10, 20], a[3:]... -> 30
+}
+
+// 出力
+// [10 20 30]			# 初期状態
+// [10 20 30 1000]		# 最後に追加
+// [10 20 30]			# 最後を削除
+// [10000 10 20 30]		# 最初に追加
+// [10 20 30]			# 最初を削除
+// [10 20 1000 30]		# インデックス2に追加
+// [10 20 30]			# インデックス2を削除
