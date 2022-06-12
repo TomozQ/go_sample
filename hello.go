41e8ae6a37ce8c3af65456ca21ee1cfa93663563
TomozQ <inf.4clover@gmail.com>
2022-06-12 19:55:04 +0900

    slice basic

--- hello.go
+++ hello.go
@@ -1,31 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"hello"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	x := hello.Input("input data")
-	// 入力された値を半角スペースで区切って配列にする ex)[10 20 30]
-	ar := strings.Split(x, " ")
-	// fmt.Println(ar)
-	t := 0
-	// 使わなくても良いとする変数は「 _ 」で表現する -> Goでは宣言された変数は使わなくてはならないため。 _ にすれば使用しなくてよい。
-	for _, v := range ar { //_ -> インデックス, v -> 要素,range ar ->呼びだされるたび配列から順番にインデックスと要素を取り出す。全てが取り出されたらforを抜ける
-
-		n, er := strconv.Atoi(v)
-
-		if er != nil {
-			goto err
-		}
-
-		t += n
-	}
-	fmt.Println("total: ", t)
-	return
-err:
-	fmt.Println("ERROR!")
+	// 配列の定義
+	a := [5]int{10, 20, 30, 40, 50}
+	// 配列からスライスを生成
+	b := a[0:3]
+	// ※スライスの定義
+	// 変数 := []型{値1, 値2 ...}
+	fmt.Println(a)
+	fmt.Println(b)
 }
