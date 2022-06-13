c4a6e99cc30d011789809b3645da5cd7482266ca
TomozQ <inf.4clover@gmail.com>
2022-06-13 22:49:37 +0900

    high order function

--- hello.go
+++ hello.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	f := func(a []string) ([]string, string) {
-		return a[1:], a[0]
+	//第一引数にstringのスライス、第二引数に関数を取る。戻り値はstringスライス。 ->第一引数で受けたスライスを第二引数の関数で処理して返却する。
+	modify := func(a []string, f func([]string) []string) []string {
+		return f(a)
 	}
 	m := []string{
-		"one",
-		"two",
-		"three",
+		"1st",
+		"2nd",
+		"3rd",
 	}
-	s := ""
-	fmt.Println(m)
-	for len(m) > 0 {
-		m, s = f(m)
-		fmt.Println(s+" ->", m)
-	}
+	fmt.Println("initial", m)
+
+	m1 := modify(m, func([]string) []string {
+		return append(m, m...)
+	})
+	fmt.Println("m1", m1)
+	m2 := modify(m, func([]string) []string {
+		return m[:len(m)-1]
+	})
+	fmt.Println("m2", m2)
+	m3 := modify(m, func([]string) []string {
+		return m[1:]
+	})
+	fmt.Println("m3", m3)
 }
