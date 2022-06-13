1f0ac0515438185b84bd21b5ea1018ef9c9fb8c0
TomozQ <inf.4clover@gmail.com>
2022-06-13 22:04:39 +0900

    variadic argument

--- hello.go
+++ hello.go
@@ -5,14 +5,13 @@ import "fmt"
 func main() {
 	m := []string{"one", "two", "three"}
 	fmt.Println(m)
-	m = insert(m, "*", 2)
-	m = insert(m, "*", 1)
+	m = push(m, "1", "2", "3")
 	fmt.Println(m)
 }
-func insert(a []string, v string, p int) (s []string) {
-	s = append(a, "")
-	s = append(s[:p+1], s[p:len(s)-1]...)
-	s[p] = v
 
+//						可変長引数
+func push(a []string, v ...string) (s []string) {
+	//			v... -> 配列vを展開したもの
+	s = append(a, v...)
 	return
 }
