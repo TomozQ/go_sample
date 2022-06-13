77f026b1e9d79c6f984859cbd5c9e688ac23e2ef
TomozQ <inf.4clover@gmail.com>
2022-06-13 23:05:16 +0900

    format outpurt

--- hello.go
+++ hello.go
@@ -3,29 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	data := "*新しい値*"
-	m1 := modify(data)
-	data = "+new data+"
-	m2 := modify(data)
-
-	fmt.Println(m1())
-	fmt.Println(m2())
+	n := 123
+	b := true
+	s := "hello"
+	fmt.Printf("number:%d, bool:%t, string:%s.", n, b, s)
 }
-func modify(d string) func() []string {
-	m := []string{
-		"1st",
-		"2nd",
-	}
-	return func() []string {
-		return append(m, d)
-	}
-}
-
-// 出力
-// [1st 2nd *新しい値*]
-// [1st 2nd +new data+]
-// m1とm2にはmodifyの戻り値である関数が代入される
-// その後m1,m2が実行された際には、modifyが実行された際のm,dの値が保持されている。
-// ^
-// |
-// クロージャ―
