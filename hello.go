941563d33d19372225bc3a823c74a19c4e82f52c
TomozQ <inf.4clover@gmail.com>
2022-06-14 23:05:51 +0900

    manipulate slices with pointers

--- hello.go
+++ hello.go
@@ -3,26 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	n := 123
-	fmt.Printf("value: %d.\n", n)
-	//値渡し
-	change1(n) //change1の引数にはnのコピーが渡されている。関数が実行された後は消滅する値。change1内で行った操作は関数外にまったく影響を与えない。
-	fmt.Printf("value: %d.\n", n)
-	//参照渡し(ポインタ渡し)
-	change2(&n)
-	fmt.Printf("value: %d.\n", n)
+	ar := []int{10, 20, 30}
+	fmt.Println(ar)
+	initial(&ar)
+	fmt.Println(ar)
 }
 
-func change1(n int) {
-	n *= 2
-}
-
-//ポインタを渡すことでそのポインタにある値が2倍に書き換えられた。
-func change2(n *int) {
-	*n *= 2
+func initial(ar *[]int) {
+	for i := 0; i < len(*ar); i++ {
+		(*ar)[i] = 0 //(*ar) -> arのポインタにあるスライスを取得できる。※*ar[i]だとar[i]にあるポインタを取得しようとしていると判断される。
+	}
 }
 
 // 出力
-// value: 123.
-// value: 123.
-// value: 246.
+// [10 20 30]
+// [0 0 0]
