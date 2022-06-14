31f643a9c122c7bfc586a8c515795573bc7e5b79
TomozQ <inf.4clover@gmail.com>
2022-06-14 22:58:11 +0900

    use a pointer as an argument

--- hello.go
+++ hello.go
@@ -4,24 +4,25 @@ import "fmt"
 
 func main() {
 	n := 123
-	//&nでnのポインタを取得
-	p := &n //ポインタを代入すると自動推論によりポインタ型と認識される。今回nはint型なので*int型というポイント型の変数となる。
-	q := &p
-	m := 10000
-	p2 := &m
-	q2 := &p2
-	fmt.Printf("q value:%d, address:%p\n", **q, *q)
-	fmt.Printf("q2 value:%d, address:%p\n", **q2, *q2)
+	fmt.Printf("value: %d.\n", n)
+	//値渡し
+	change1(n) //change1の引数にはnのコピーが渡されている。関数が実行された後は消滅する値。change1内で行った操作は関数外にまったく影響を与えない。
+	fmt.Printf("value: %d.\n", n)
+	//参照渡し(ポインタ渡し)
+	change2(&n)
+	fmt.Printf("value: %d.\n", n)
+}
 
-	pb := p
-	p = p2
-	p2 = pb
-	fmt.Printf("q value:%d, address:%p\n", **q, *q)
-	fmt.Printf("q2 value:%d, address:%p\n", **q2, *q2)
+func change1(n int) {
+	n *= 2
+}
+
+//ポインタを渡すことでそのポインタにある値が2倍に書き換えられた。
+func change2(n *int) {
+	*n *= 2
 }
 
 // 出力
-// q value:123, address:0xc000012088
-// q2 value:10000, address:0xc0000120a0
-// q value:10000, address:0xc0000120a0
-// q2 value:123, address:0xc000012088
+// value: 123.
+// value: 123.
+// value: 246.
