3c31debc3862faab0af5bb8d4cbf30a46b5dcadd
TomozQ <inf.4clover@gmail.com>
2022-06-14 22:41:55 +0900

    ponter of pointer of ...

--- hello.go
+++ hello.go
@@ -6,20 +6,22 @@ func main() {
 	n := 123
 	//&nでnのポインタを取得
 	p := &n //ポインタを代入すると自動推論によりポインタ型と認識される。今回nはint型なので*int型というポイント型の変数となる。
+	q := &p
 	m := 10000
 	p2 := &m
-	fmt.Printf("p value:%d, address:%p\n", *p, p)
-	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+	q2 := &p2
+	fmt.Printf("q value:%d, address:%p\n", **q, *q)
+	fmt.Printf("q2 value:%d, address:%p\n", **q2, *q2)
 
 	pb := p
 	p = p2
 	p2 = pb
-	fmt.Printf("p value:%d, address:%p\n", *p, p)
-	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+	fmt.Printf("q value:%d, address:%p\n", **q, *q)
+	fmt.Printf("q2 value:%d, address:%p\n", **q2, *q2)
 }
 
 // 出力
-// p value:123, address:0xc000012088
-// p2 value:10000, address:0xc0000120a0
-// p value:10000, address:0xc0000120a0
-// p2 value:123, address:0xc000012088
+// q value:123, address:0xc000012088
+// q2 value:10000, address:0xc0000120a0
+// q value:10000, address:0xc0000120a0
+// q2 value:123, address:0xc000012088
