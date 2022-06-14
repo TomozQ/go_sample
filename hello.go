73a0bfc2cef1eaf2a583113fd8923d99b8560359
TomozQ <inf.4clover@gmail.com>
2022-06-14 22:24:59 +0900

    pointer basic

--- hello.go
+++ hello.go
@@ -4,7 +4,15 @@ import "fmt"
 
 func main() {
 	n := 123
-	b := true
-	s := "hello"
-	fmt.Printf("number:%d, bool:%t, string:%s.", n, b, s)
+	//&nでnのポインタを取得
+	p := &n //ポインタを代入すると自動推論によりポインタ型と認識される。今回nはint型なので*int型というポイント型の変数となる。
+	fmt.Println("number: ", n)
+	fmt.Println("pointer: ", p)
+	//pは自動推論によりポインタ型とされているので*pとして値を取得する。
+	fmt.Println("value: ", *p)
 }
+
+// 出力
+// number:  123
+// pointer:  0xc000012088
+// value:  123
