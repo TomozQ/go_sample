da5b307296d14f661eb339adabec4e4bc08f667c
TomozQ <inf.4clover@gmail.com>
2022-06-15 18:53:16 +0900

    struct define as type

--- hello.go
+++ hello.go
@@ -2,21 +2,22 @@ package main
 
 import "fmt"
 
-var mydata struct {
+type Mydata struct {
 	Name string
 	Data []int
 }
 
 func main() {
-	//構造体
-	// struct{
-	// 	...変数宣言...
-	// }
+	taro := Mydata{
+		"Taro",
+		[]int{10, 20, 30},
+	}
 
-	// 変数を指定した定義の構造体の値として宣言できる。
-	// var 変数 struct{......}
-	// 利用する際には 構造体.変数
-	mydata.Name = "Taro"
-	mydata.Data = []int{10, 20, 30}
-	fmt.Println(mydata)
+	hanako := Mydata{
+		"Hanako",
+		[]int{90, 80, 70},
+	}
+
+	fmt.Println(taro)
+	fmt.Println(hanako)
 }
