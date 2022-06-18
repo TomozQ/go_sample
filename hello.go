ab9355bfb5deec5442b244e74e827a3ef7fc423b
TomozQ <inf.4clover@gmail.com>
2022-06-19 00:29:39 +0900

    channel management by select

--- hello.go
+++ hello.go
@@ -2,30 +2,71 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func total (cs chan int, cr chan int) {
-	n := <- cs
-	fmt.Println("n = ", n)
-	t := 0
+// select{
+// case 文:
+// 	...実行する処理...
+// case 文:
+// 	...実行する処理...
+// case 文:
+// 	...実行する処理...
+// default:
+// 	...全てに当てはまらない場合の処理...
+// }
+
+// switchと違いcaseには値ではなく操作の文を指定する。基本的には「<-チャンネル」という形でチャンネルから値を取り出す文を記述しておく。（チャンネルから値を取り出せたcaseに進む）
+
+func count (n int, s int, c chan int) {
 	for i := 1; i <= n; i++ {
-		t += i
+		c <- i
+		time.Sleep(time.Duration(s) * time.Millisecond)
 	}
-	cr <- t
 }
 
 func main() {
-	cs := make(chan int)
-	cr := make(chan int)
-	go total(cs, cr)
-	cs <- 100
-	fmt.Println("total: ", <-cr)
+	n1, n2, n3 := 3, 5, 10
+	m1, m2, m3 := 100, 75, 50
+	c1 := make(chan int)
+	go count(n1, m1, c1)
+	c2 := make(chan int)
+	go count(n2, m2, c2)
+	c3 := make(chan int)
+	go count(n3, m3, c3)
+
+	for i := 0; i < n1 + n2 + n3; i++ {
+		select{
+		case re := <- c1:
+			fmt.Println("*  first", re)
+		
+		case re := <- c2:
+			fmt.Println("** second", re)
+		
+		case re := <- c3:
+			fmt.Println("***third", re)
+		}
+	}
+	fmt.Println("*** finish. ***")
 }
 
 // 出力
-// n =  100
-// total:  5050
-
-// csを「main -> total」
-// crを「total -> main」
-// というように2つのチャンネルで双方向に値を送受している。 
+// ***third 1
+// ** second 1
+// *  first 1
+// ***third 2
+// ** second 2
+// ***third 3
+// *  first 2
+// ***third 4
+// ** second 3
+// ***third 5
+// *  first 3
+// ** second 4
+// ***third 6
+// ***third 7
+// ** second 5
+// ***third 8
+// ***third 9
+// ***third 10
+// *** finish. ***
