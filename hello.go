9d63fff6f051973ca5256325995a5b07459697db
TomozQ <inf.4clover@gmail.com>
2022-06-18 23:51:36 +0900

    main to channel

--- hello.go
+++ hello.go
@@ -1,31 +1,23 @@
 package main
 
-import(
+import (
 	"fmt"
+	"time"
 )
 
-func total(n int, c chan int){
+func total(c chan int){
+	n := <-c  // チャンネルは、値がまだ用意されていない場合には、送られてくるまで処理を待つ。 -> 値が得られないというエラーになることはない。
+	fmt.Println("n = ", n)
 	t := 0
-	for i := 1; i <= n; i++{
+	for i := 1; i <= n; i++ {
 		t += i
-	} 
-	// チャンネルに t を格納する
-	c <- t
+	}
+	fmt.Println("total: ", t)
 }
 
 func main() {
-	// int型を格納するチャンネルを生成
 	c := make(chan int)
-	// total関数を並列処理で実行
-	go total(1000, c)
-	go total(100, c)
-	go total(10, c)
-	
-	x, y, z := <-c, <-c, <-c // <-cを呼びだすごとにチャンネルから値が取り出されている。3回実行することで全ての値を取り出せる。
-	fmt.Println(x, y, z)
+	go total(c)
+	c <- 100 // チャンネルは複数のスレッド間で値をやり取りするためのものなので、双方で準備ができていないとならない => Goルーチンを実行した後でないとチャンネルは使えない。
+	time.Sleep(100 * time.Millisecond)
 }
-
-// 出力
-// 55 500500 5050
-// 実行したスレッドのかかる時間によりチャンネルに値が入る順番は変わる。
-// Goルーチンを呼びだした順に値が追加されるわけでは無い。
