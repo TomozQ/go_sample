5c86b3580731327c2c87294062c9647a4d1a7c89
TomozQ <inf.4clover@gmail.com>
2022-06-18 22:57:35 +0900

    Go routin basic

--- hello.go
+++ hello.go
@@ -1,67 +1,19 @@
 package main
 
-import (
+import(
 	"fmt"
-	"reflect"
+	"time"
 )
 
-// General is all type data
-type General interface{}
-
-// GData is holding General value
-type GData interface {
-	Set(nm string, g General) GData
-	Print()
-}
-
-// NData is structure
-type NData struct {
-	Name string
-	Data []int
-}
-
-// Set is NData method
-func (nd *NData) Set(nm string, g General) GData {
-	nd.Name = nm
-	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf(0)){ //reflect.TypeOf(0) -> int型
-		nd.Data = g.([]int) //型アサーション
+func hello(s string, n int){
+	for i := 1; i <= 10; i++{
+		fmt.Printf("<%d %s>", i, s)
+		time.Sleep(time.Duration(n) * time.Millisecond)
 	}
-	return nd
-}
-
-// Print is NData method
-func (nd *NData)Print(){
-	fmt.Printf("<<%s>> value: %d\n", nd.Name, nd.Data)
-}
-
-// SData is structure
-type SData struct {
-	Name string
-	Data []string
-}
-
-// Set is SData method
-func (sd *SData) Set(nm string, g General) GData {
-	sd.Name = nm
-	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf("")){ // reflect.TypeOf("") -> string型
-		sd.Data = g.([]string) //型アサーション
-	}
-	return sd
-}
-
-// Print is SData method
-func (sd *SData)Print(){
-	fmt.Printf("* %s [%s] *\n", sd.Name, sd.Data)
 }
 
 func main() {
-	var data = []GData{}
-	data = append(data, new(NData).Set("Taro", []int{1, 2, 3}))
-	data = append(data, new(SData).Set("Jiro", []string{"hello", "bye"}))
-	data = append(data, new(NData).Set("Hanako", 98700))
-	data = append(data, new(SData).Set("Sachiko", "happy?"))
-
-	for _, ob := range data{
-		ob.Print()
-	}
+	go hello("hello", 50)
+	hello("bye!", 100)
+	// hello("hello", 50)
 }
